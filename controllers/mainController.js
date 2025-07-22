afb18bd426f8ec1174fcc1556f807798d7d32c7c
gabeLin300 <gabelinares300@gmail.com>
2025-07-22 19:34:10 -0400

    refactored the landing page endpoint to use mongoose odm

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,7 +2,7 @@ const model = require('../models/event')
 
 //GET home page
 exports.index = (req,res) => {
-    model.findSorted()
+    model.Event.find().sort({views:-1})
     .then(events=> {
         if(events) {
             top3events = events.slice(0,3);
