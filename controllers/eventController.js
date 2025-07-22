2c0ed19d01c6199024b008f3d503776f56b3f84e
gabeLin300 <gabelinares300@gmail.com>
2025-07-22 18:52:09 -0400

    added controller functionality to handle all requests

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -4,14 +4,18 @@ const { v4: uuidv4 } = require('uuid');
 
 //GET all events
 exports.index = (req, res, next) => {
-    eventMap = model.getMap();
-    if (eventMap.size) {
-        res.render('event/index', {eventMap});
-    } else {
-        let err = new Error('Cannot find any events');
-        err.status = 404;
-        next(err);
-    }
+    model.find()
+    .then(events => {
+        if (events) {
+            eventMap = Map.groupBy(events, event => event.category);
+            res.render('event/index', {eventMap});
+        } else {
+            let err = new Error('Cannot find any events');
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err => next(err));
 }
 
 //GET form to add a new event
@@ -20,47 +24,59 @@ exports.new =  (req, res) => {
 }
 
 //POST new event
-exports.create = (req,res) => {
+exports.create = (req,res, next) => {
     newEvent = req.body;
     eventImg = '/uploads/' + req.file.filename;
-    dbEvent = model.createNew(newEvent, eventImg);
-    if (dbEvent) {
-        res.redirect('/events/'+dbEvent.id);
-    }
+    newEvent.eventImg = eventImg;
+    newEvent.views = 0;
+    model.createNew(newEvent)
+    .then(result=> {
+        res.redirect('/events/'+result.insertedId.toString());
+    })
+    .catch(err=>next(err));
 }
 //GET event by id
 exports.show = (req, res, next) => {
     let id = req.params.id;
-    currEvent = model.findById(id);
-    if (currEvent) {
-        // simple cookie for incrementing views to an event
-        //followed express documentation
-        if(!req.cookies[`event${id}`]) {
-            currEvent.views += 1;
-            cookieTok = uuidv4();
-            //cookie expires in 10 seconds for testing
-            res.cookie(`event${id}`, cookieTok, {maxAge: 10000, httpOnly: true});
+    let currEvent;
+    model.findById(id)
+    .then(dbEvent => {
+        if (dbEvent) {
+            currEvent = dbEvent
+            //simple cookies for tracking views on an event
+            if(!req.cookies[`event${id}`]) {
+                cookieTok = uuidv4();
+                //cookie expires in 10 seconds for testing
+                res.cookie(`event${id}`, cookieTok, {maxAge: 10000, httpOnly: true});
+                return model.incrementViewsById(id);
+            } 
+            return Promise.resolve();
+        } else {
+            let err = new Error('Cannot find the event with id: '+id);
+            err.status = 404;
+            next(err);
         }
+    })
+    .then(result => {
         res.render('event/show', {currEvent});
-        
-    } else {
-        let err = new Error('Cannot find the event with id: '+id);
-        err.status = 404;
-        next(err);
-    }
+    })
+    .catch(err => next(err));
 }
 //GET edit form for specific event
 exports.edit = (req, res, next) => {
     let id = req.params.id;
-    currEvent = model.findById(id);
-    eventCategories = model.getEventCategories();
-    if (currEvent) {
-        res.render('event/edit', {currEvent, eventCategories});
-    } else {
-        let err = new Error('Cannot find the event with id: '+id);
-        err.status = 404;
-        next(err);
-    }
+    model.findById(id)
+    .then(currEvent => {
+        eventCategories = model.getEventCategories();
+        if (currEvent) {
+            res.render('event/edit', {currEvent, eventCategories});
+        } else {
+            let err = new Error('Cannot find the event with id: '+id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err));
 }
 //PUT update an event by id
 exports.update = (req, res, next) => {
@@ -68,22 +84,30 @@ exports.update = (req, res, next) => {
     newEvent = req.body;
     if(req.file.filename)
         newEvent.eventImg = '/uploads/' + req.file.filename;
-    if (model.updateById(id, newEvent)) {
-        res.redirect('/events/'+id);
-    } else {
-        let err = new Error('Cannot find the event with id: '+id);
-        err.status = 404;
-        next(err);
-    }
+    model.updateById(id, newEvent)
+    .then(result=> {
+        if(result.modifiedCount === 1) {
+            res.redirect('/events/'+id);
+        } else {
+            let err = new Error('Could not update the event with id: '+id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err));
 }
 //DELETE event by id
 exports.delete = (req, res, next) => {
     id = req.params.id
-    if (model.deleteById(id)) {
-        res.redirect('/events');
-    } else {
-        let err = new Error('Cannot find the event with id: '+id);
-        err.status = 404;
-        next(err);
-    }
+    model.deleteById(id)
+    .then(result=> {
+        if (result.deletedCount === 1) {
+            res.redirect('/events');
+        } else {
+            let err = new Error('Could not delete the event with id: '+id);
+            err.status = 404;
+            next(err);
+        }
+    })
+    .catch(err=>next(err));
 }
