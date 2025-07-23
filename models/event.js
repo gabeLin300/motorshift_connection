6509b766eb36c836c43fc33edc3e4468b03f6d45
gabeLin300 <gabelinares300@gmail.com>
2025-07-23 15:19:16 -0400

    feat: added date input validation to handle users submitting an event end date that is less than the start date.

--- models/event.js
+++ models/event.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { validate } = require('uuid');
 const Schema = mongoose.Schema;
 
 //available categories
@@ -8,12 +9,18 @@ const eventSchema = new Schema({
         name: {type: String, required: [true, 'Name is required'], min: [10,'Must be atleast 10 chars'], max:[80, 'Must be atmost 80 chars']},
         host: {type: String, required: [true, 'Host is required'], min: [10,'Must be atleast 10 chars'], max: [80,'Must be atleast 10 chars']},
         start: {type: Date, required: [true, 'Start date/time is required'], min: [Date.now(), 'The start date must be current']},
-        end: {type: Date, required: [true, 'End date/time is required'], min: [Date.now(), 'The end date must be current']},
+        end: {type: Date, 
+              required: [true, 'End date/time is required'], 
+              min: [Date.now(), 'The end date must be current'],
+              validate: {
+                        validator: function(newEndValue) {return newEndValue > this.get('start')},
+                        message: 'End date must be greater than Start date.'
+                }},
         location: {type: String, required: [true, 'Location is required'], min: [10, 'The address length must be atleast 10']},
         details: {type:String, required: [true, 'Details are required'], min: [50, 'Event details must be atleast 50 chars']},
         category: {type: String, enum: {values: categories, message: '{VALUE} is not supported'}},
         eventImg: {type: String, required: [true, 'An uploaded image path is required']},
-        views: {type: Number, min: 0, default: 0}},
+        views: {type: Number, min: 0, default: 0, required: [true]}},
         {timestamps: true}
 );
 
