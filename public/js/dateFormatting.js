0cd7a7f6bf497cef189c8b5c6846ad51ec2b467a
gabeLin300 <gabelinares300@gmail.com>
2025-07-23 13:10:51 -0400

    removed old date formatting javascript from client

--- public/js/dateFormatting.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    //simple date object to format in a more readable way to user
-    const eventDate = document.querySelectorAll(".event-time");
-    if(eventDate) {
-        eventDate.forEach(element => {
-            element.textContent = new Date(element.textContent).toString();
-        })
-    }
-});
