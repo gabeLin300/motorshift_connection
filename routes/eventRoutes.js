2a671859017b4b6eada743109a9f0e874742d363
gabeLin300 <gabelinares300@gmail.com>
2025-09-16 16:20:16 -0400

    Added validators to all routes

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -2,26 +2,31 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/eventController')
 const {fileUpload} = require('../middleware/fileUpload');
+const {isAuthenticated, isEventOwner, isNotEventOwner} = require('../middleware/auth');
+const {isValidId, eventValidator, rsvpValidator, validationResults} = require('../middleware/validators');
 
 //GET all events
 router.get('/', controller.index);
 
 //GET form to add a new event
-router.get('/new', controller.new);
+router.get('/new', isAuthenticated, controller.new);
 
 //POST a new event
-router.post('/', fileUpload, controller.create);
+router.post('/', isAuthenticated, fileUpload, eventValidator, validationResults, controller.create);
 
 //GET event by id
-router.get('/:id', controller.show);
+router.get('/:id', isValidId, controller.show);
 
 //GET edit form for specific event
-router.get('/:id/edit', controller.edit);
+router.get('/:id/edit', isValidId, isAuthenticated, isEventOwner, controller.edit);
 
 //PUT update an event by id
-router.put('/:id', fileUpload, controller.update);
+router.put('/:id', isValidId, isAuthenticated, isEventOwner, fileUpload, eventValidator, validationResults, controller.update);
 
 //DELETE event by id
-router.delete('/:id', controller.delete);
+router.delete('/:id', isValidId, isAuthenticated, isEventOwner, controller.delete);
+
+//POST a new RSVP to specific event
+router.post('/:id/rsvp', isValidId, isAuthenticated, isNotEventOwner, rsvpValidator, validationResults, controller.rsvp)
 
 module.exports = router;
